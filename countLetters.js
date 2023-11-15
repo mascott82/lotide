d75bc7453f19aebed6a1e65050758026633f9aae
mascott82 <m.a.scott@live.com>
2023-11-14 23:02:19 -0600

    Implementing the function countLetters().

--- countLetters.js
+++ countLetters.js
@@ -23,5 +23,15 @@ const countLetters = function(str) {
 };
 
 
-console.log(countLetters("lighthouse in the house"));
-assertEqual(countLetters("lighthouse in the house"), {l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1});
+let result = countLetters("lighthouse in the house");
+
+assertEqual(result["l"], 1);
+assertEqual(result["i"], 2);
+assertEqual(result["g"], 1);
+assertEqual(result["h"], 4);
+assertEqual(result["t"], 2);
+assertEqual(result["o"], 2);
+assertEqual(result["u"], 2);
+assertEqual(result["s"], 2);
+assertEqual(result["e"], 3);
+assertEqual(result["n"], 1);
