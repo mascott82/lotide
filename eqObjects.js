65f9cf8bf37545975353db82208f1200149b3b51
mascott82 <m.a.scott@live.com>
2023-11-15 09:06:18 -0600

    Implementing the function eqObjects().

--- eqObjects.js
+++ eqObjects.js
@@ -30,8 +30,9 @@ const eqObjects = function(obj1, obj2) {
   for (let i = 0; i < keys1.length; i++) {
     if (Array.isArray(obj1[keys1[i]]) && Array.isArray(obj2[keys1[i]])) {
       if (!eqArrays(obj1[keys1[i]], obj2[keys1[i]])) return false;
+    } else {
+      if (obj1[keys1[i]] !== obj2[keys1[i]]) return false;
     }
-    if (obj1[keys1[i]] !== obj2[keys1[i]]) return false;
   }
   
   return true;
