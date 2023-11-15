ef634906328806ba2c7e810702c342117aae688c
mascott82 <m.a.scott@live.com>
2023-11-15 09:20:27 -0600

    Implementing the function assertObjectsEqual().

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,17 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
 const eqObjects = function(obj1, obj2) {
   let keys1 = Object.keys(obj1);
   let keys2 = Object.keys(obj2);
@@ -5,7 +19,11 @@ const eqObjects = function(obj1, obj2) {
   if (keys1.length !== keys2.length) return false;
 
   for (let i = 0; i < keys1.length; i++) {
-    if (obj1[keys1[i]] !== obj2[keys1[i]]) return false;
+    if (Array.isArray(obj1[keys1[i]]) && Array.isArray(obj2[keys1[i]])) {
+      if (!eqArrays(obj1[keys1[i]], obj2[keys1[i]])) return false;
+    } else {
+      if (obj1[keys1[i]] !== obj2[keys1[i]]) return false;
+    }
   }
   
   return true;
