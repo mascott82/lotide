39bc9f22b7caded0c4a9c629b0f3e9199940a65e
mascott82 <m.a.scott@live.com>
2023-11-16 19:12:55 -0600

    Adjusting the function eqArrays().

--- eqArrays.js
+++ eqArrays.js
@@ -13,15 +13,23 @@ const eqArrays = function(arr1, arr2) {
   }
 
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+      //return eqArrays(arr1[i], arr2[i]);
+    } else {
+      return arr1[i] === arr2[i];
     }
   }
-
   return true;
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
 assertEqual(eqArrays([1, 2], [1, 2, 3]), false);
-assertEqual(eqArrays([], []), true)
+assertEqual(eqArrays([], []), true);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
