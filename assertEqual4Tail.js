32faff80f2d9e11b89b5ab5e9536895907927550
mascott82 <m.a.scott@live.com>
2023-11-19 22:21:00 -0600

    Refactoring the functions.

---
+++ assertEqual4Tail.js
@@ -0,0 +1,26 @@
+// FUNCTION IMPLEMENTATION
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+
+const assertEqual = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    console.log(`Assertion Failed: Arrays have different lengths`);
+    return;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: Element at index ${i} does not match`);
+      return;
+    }
+  }
+
+  console.log(`✅✅✅ Assertion Passed: Arrays are equal`);
+};
+
+module.exports = assertEqual;
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,13 @@
 const tail = require('../tail');
+const assertEqual = require('../assertEqual4Tail');
 
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
-tail.assertEqual(words.length, 3);
+assertEqual(tail(words).length, 3);
 
 // Test cases
-tail.assertEqual(tail([5, 6, 7]), [6, 7]);
-tail.assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-tail.assertEqual(tail([42]), []); // An array with only one element should yield an empty array as its tail
-tail.assertEqual(tail([]), []); // An empty array should yield an empty array as its tail
+assertEqual(tail([5, 6, 7]), [6, 7]);
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+assertEqual(tail([42]), []); // An array with only one element should yield an empty array as its tail
+assertEqual(tail([]), []); // An empty array should yield an empty array as its tail
